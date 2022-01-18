8a3f7361a6c21c7c4c4dbf1e4b9da072663731b3
HoHyun95 <suhoh01@naver.com>
2022-01-18 14:05:29 +0900

    동호회 게시판 대댓글 기능 초안

--- src/main/java/kh/hobby1st/dao/ClubBoardDAO.java
+++ src/main/java/kh/hobby1st/dao/ClubBoardDAO.java
@@ -24,25 +24,25 @@ public class ClubBoardDAO {
 
 	// 해당 동호회 게시판 총 개수
 	public int getRecordCount(int seq) {
-		return mybatis.selectOne("ClubBoard.RecordCount",seq);
+		return mybatis.selectOne("ClubBoard.RecordCount", seq);
 	}
-	
+
 	// 검색 시 총 개수
 	public int getRecordSearchCount(int club_board_id, String keyword, String searchWord) {
-		
+
 		Map<String, String> map = new HashMap<>();
 
 		map.put("searchWord", String.valueOf(searchWord));
 		map.put("club_board_id", String.valueOf(club_board_id));
-		
+
 		int count = 0;
 		if (keyword.equals("제목")) {
 			count = mybatis.selectOne("ClubBoard.searchTitleCount", map);
-		}else if(keyword.equals("작성자")) {
+		} else if (keyword.equals("작성자")) {
 			count = mybatis.selectOne("ClubBoard.searchWriterCount", map);
 		}
 		return count;
-		
+
 	}
 
 	// 게시판 리스트 페이징하여 출력
@@ -56,29 +56,30 @@ public class ClubBoardDAO {
 
 		return mybatis.selectList("ClubBoard.selectBoard", map);
 	}
-	
+
 	// 게시판 상세보기
 	public ClubBoardDTO boardDetail(int board_seq) {
-		return mybatis.selectOne("ClubBoard.boardDetail",board_seq);
+		return mybatis.selectOne("ClubBoard.boardDetail", board_seq);
 	}
-	
+
 	// 게시판 조회수 증가
 	public int increaseView(int reply_seq) {
-		return mybatis.update("ClubBoard.increaseView",reply_seq);
+		return mybatis.update("ClubBoard.increaseView", reply_seq);
 	}
-	
+
 	// 게시판 삭제
 	public int deleteBoard(int board_seq) {
 		return mybatis.delete("ClubBoard.deleteBoard", board_seq);
 	}
-	
+
 	// 게시판 수정
 	public int modifyBoard(ClubBoardDTO dto) {
 		return mybatis.update("ClubBoard.modifyBoard", dto);
 	}
-	
+
 	// 게시판 제목 검색
-	public List<ClubBoardDTO> selectBoardSearchByPaging(int start, int end, int seq, String keyword, String searchWord) {
+	public List<ClubBoardDTO> selectBoardSearchByPaging(int start, int end, int seq, String keyword,
+			String searchWord) {
 
 		Map<String, String> map = new HashMap<>();
 
@@ -86,26 +87,60 @@ public class ClubBoardDAO {
 		map.put("end", String.valueOf(end));
 		map.put("club_board_id", String.valueOf(seq));
 		map.put("searchWord", searchWord);
-		
+
 		List<ClubBoardDTO> list = new ArrayList<ClubBoardDTO>();
 		if (keyword.equals("제목")) {
 			list = mybatis.selectList("ClubBoard.searchTitle", map);
-		}else if(keyword.equals("작성자")) {
+		} else if (keyword.equals("작성자")) {
 			list = mybatis.selectList("ClubBoard.searchWriter", map);
 		}
 		return list;
 	}
+
+	// ------------ 추천 기능 -------------------
+
+	// 추천 여부 확인
+	public int checkRec(int cb_seq, String rec_id) {
+
+		Map<String, String> map = new HashMap<>();
+
+		map.put("cb_seq", String.valueOf(cb_seq));
+		map.put("rec_id", rec_id);
+
+		return mybatis.selectOne("ClubBoardRec.checkRec", map);
+	}
+
+	// 추천한 게시글에 id 저장
+	public int insertRec(int cb_seq, String rec_id) {
+
+		Map<String, String> map = new HashMap<>();
+
+		map.put("cb_seq", String.valueOf(cb_seq));
+		map.put("rec_id", rec_id);
+
+		return mybatis.insert("ClubBoardRec.insertRec", map);
+	}
+
+	// 추천 쉬소
+	public int deleteRec(int cb_seq, String rec_id) {
+
+		Map<String, String> map = new HashMap<>();
+
+		map.put("cb_seq", String.valueOf(cb_seq));
+		map.put("rec_id", rec_id);
+
+		return mybatis.insert("ClubBoardRec.deleteRec", map);
+	}
 	
+	// 추천수 갱신
+	public int updateRec(int cb_seq) {
+		return mybatis.update("ClubBoardRec.updateRec", cb_seq);
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	// 추천수
+	public int recCount(int cb_seq) {
+		return mybatis.selectOne("ClubBoardRec.recCount", cb_seq);
+	}
 	
 
 }

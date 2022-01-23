335f9f724294b99adbb3f341ab91d868f1abc37d
HoHyun95 <suhoh01@naver.com>
2022-01-23 15:58:50 +0900

    가입 상태DTO

---
+++ src/main/java/kh/hobby1st/dto/ClubJoinStateDTO.java
@@ -0,0 +1,12 @@
+package kh.hobby1st.dto;
+
+public class ClubJoinStateDTO {
+	
+	private int cs_seq;
+	private int cs_board_seq;
+	private String cs_club_name;
+	private String cs_boss_name;
+	private String cs_join_name;
+	private int cs_state;
+
+}

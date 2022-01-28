162abff5eba170fdafab2b5ae53ce3b04d64eb00
HoHyun95 <suhoh01@naver.com>
2022-01-28 14:32:39 +0900

    vote mapper add

--- src/main/java/kh/hobby1st/controller/VoteController.java
+++ src/main/java/kh/hobby1st/controller/VoteController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kh.hobby1st.dto.VoteListDTO;
+import kh.hobby1st.dto.VoteOptionDTO;
 import kh.hobby1st.service.VoteService;
 
 @Controller
@@ -21,12 +23,17 @@ public class VoteController {
 	
 	// 투표 작성 페이지 이동
 	@RequestMapping("writePage")
-	public String voteWrite() {
+	public String writePage() {
 		return "vote/voteWrite";
 	}
 	
 	// 투표 작성 하기
-	
+	@RequestMapping("voteWrite")
+	public String voteWrite(VoteListDTO Ldto, VoteOptionDTO Odto) {
+		
+		System.out.println(Ldto.getVl_type());
+		return "vote/voteWrite";
+	}
 	
 	
 }

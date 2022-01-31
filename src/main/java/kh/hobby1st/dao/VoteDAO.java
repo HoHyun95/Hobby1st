390474e666761d02a81633cb5f63c3972ee467f6
HoHyun95 <suhoh01@naver.com>
2022-02-01 03:53:03 +0900

    vote dao update

--- src/main/java/kh/hobby1st/dao/VoteDAO.java
+++ src/main/java/kh/hobby1st/dao/VoteDAO.java
@@ -1,12 +1,15 @@
 package kh.hobby1st.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kh.hobby1st.dto.VoteListDTO;
+import kh.hobby1st.dto.VoteOptionDTO;
 
 @Repository
 public class VoteDAO {
@@ -27,6 +30,33 @@ public class VoteDAO {
 	// 투표 리스트 (전쳬)
 	public List<VoteListDTO> selectVoteAll() {
 		return mybatis.selectList("Vote.selectVoteAll");
+	}
+	
+	// 해당 투표 정보 가져오기
+	public List<VoteListDTO> selectVoteDetail(int vl_seq) {
+		return mybatis.selectList("Vote.selectVoteDetail", vl_seq);
+	}
+	
+	
+	// 해당 투표 옵션 값들 가져오기
+	public List<VoteOptionDTO> selectVoteOption(int vo_vote_seq) {
+		return mybatis.selectList("Vote.selectVoteOption", vo_vote_seq);
+	}
+
+	// 해당 동호회에 대한 투표 여부
+	public int voteCheck(int vc_vote_seq, String vc_vote_id) {
+		
+		Map<String, String> map = new HashMap<>();
+
+		map.put("vc_vote_seq", String.valueOf(vc_vote_seq));
+		map.put("vc_vote_id", vc_vote_id);
+		
+		return mybatis.selectOne("Vote.voteCheck", map);
+	}
+	
+	// 투표 목록들에 대한 나의 투표 참여 여부 리스트
+	public List<VoteListDTO> voteCheckList(String vc_vote_id) {
+		return mybatis.selectList("Vote.voteCheckList", vc_vote_id);
 	}
 
 }

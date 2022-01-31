96a233ad290fac247f8b465ca487d1d0cebcd01b
HoHyun95 <suhoh01@naver.com>
2022-02-01 04:01:13 +0900

    vote con update

--- src/main/java/kh/hobby1st/controller/VoteController.java
+++ src/main/java/kh/hobby1st/controller/VoteController.java
@@ -34,17 +34,30 @@ public class VoteController {
 	
 	// 투표 작성 페이지 이동
 	@RequestMapping("detailPage")
-	public String detailPage() {
+	public String detailPage(int vl_seq, Model model) {
 		
+		String mem_id = (String) session.getAttribute("mem_id");
+		
+		List<VoteListDTO> voteDetail = voteService.selectVoteDetail(vl_seq);
+		List<VoteOptionDTO> voteOption = voteService.selectVoteOption(vl_seq);
+		int voteCheck = voteService.voteCheck(vl_seq, mem_id);
+		
+		model.addAttribute("voteDetail", voteDetail);
+		model.addAttribute("voteOption", voteOption);
+		model.addAttribute("voteCheck", voteCheck);
 		return "vote/voteDetail";
 	}
 	
 	// 투표 리스트 이동
 	@RequestMapping("listPage")
 	public String listPage(Model model) {
+		
+		String mem_id = (String) session.getAttribute("mem_id");
+		
 		List<VoteListDTO> vote_list = voteService.selectVoteAll();
+		List<VoteListDTO> voteCheckList = voteService.voteCheckList(mem_id);
 		
-		
+		model.addAttribute("voteCheckList", voteCheckList);
 		model.addAttribute("vote_list", vote_list);
 		return "vote/voteList";
 	}
